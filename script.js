8193f57540fa631f33a813f54aeb42e800b1b027
Anil Rai <anilrai4001@gmail.com>
2023-08-01 01:11:21 +0530

    done

--- script.js
+++ script.js
@@ -1,64 +1,95 @@
-const uppercaseAlphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const lowercaseAlphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?', '`', '~'];
+const display = document.getElementById('password-display');
+const inputlength = document.getElementById('length');
+const displaylength = document.getElementById('length-number');
 
-const upper = document.querySelector('#upper');
-const lower = document.querySelector('#lower');
-const number = document.querySelector('#number');
-const symbol = document.querySelector('#symbol');
+const uppercase = document.getElementById('uppercase');
+const lowercase = document.getElementById('lowercase');
+const numbers = document.getElementById('numbers');
+const symbols = document.getElementById('symbols');
 
-let includeUpper=false;
-let includeLower=false;
-let includeNumber=false;
-let includeSymbol=false;
+const boxes = document.getElementsByClassName('boxes');
 
-upper.addEventListener('click',(e)=>{
-    includeUpper = !includeUpper;
-    console.log(includeUpper);
-});
-lower.addEventListener('click',(e)=>{
-    includeLower = !includeLower;
-    console.log(includeLower);
-});
-number.addEventListener('click',(e)=>{
-    includeNumber = !includeNumber;
-    console.log(includeNumber);
-});
-symbol.addEventListener('click',(e)=>{
-    includeSymbol = !includeSymbol;
-    console.log(includeSymbol);
-});
+const copy = document.getElementById('copy');
 
+const btn = document.getElementById('generate');
 
-let s = "test";
-let num=10;
-while(num>10){
-    if(num>0 && includeUpper){
-        let index = Math.floor(Math.random()*26);
-        s += uppercaseAlphabets[index];
-        console.log(s);
-        --num;
-    }
-    if(num>0 && includeLower){
-        let index = Math.floor(Math.random()*26);
-        s += lowercaseAlphabets[index];
-        console.log(s);
-        --num;
-    }
-    if(num>0 && includeNumber){
-        let index = Math.floor(Math.random()*10);
-        s += numbersArray[index];
-        console.log(s);
-        --num;
-    }
-    if(num>0 && includeSymbol){
-        let index = Math.floor(Math.random()*(symbolsArray.length));
-        s += symbolsArray[index];
-        console.log(s);
-        --num;
-    }
-    num--;
+displaylength.innerText = inputlength.value;
+inputlength.addEventListener('input',()=>{
+    displaylength.innerText = inputlength.value;
+})
+
+function getChar(s){
+    return s.charAt(Math.floor(Math.random()*s.length));
 }
 
-console.log(s);
+let uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
+let numbersSet = "1234567890";
+let symbolsSet = "~!@#$%^&*()";
+
+function generatePassword(){
+
+    for(let i=0;i<boxes.length;i++){
+        boxes[i].style.backgroundColor = "black";
+    }
+
+    let password = "";
+
+    let strengthCount =0;
+    if(uppercase.checked)
+        strengthCount++;
+    if(lowercase.checked)
+        strengthCount++;
+    if(numbers.checked)
+        strengthCount++;
+    if(symbols.checked)
+        strengthCount++;
+
+    let len=displaylength.innerText;
+
+    if(len<4)
+        return password;
+    while(len>0){
+        if(!uppercase.checked && !lowercase.checked && !numbers.checked && !symbols.checked){
+            break;
+        }
+        if(uppercase.checked && len>0){
+            password += getChar(uppercaseSet);
+            len--;
+
+        }
+        if(lowercase.checked && len>0){
+            password += getChar(lowercaseSet);
+            len--;
+        }
+        if(numbers.checked && len>0){
+            password += getChar(numbersSet);
+            len--;
+        }
+        if(symbols.checked && len>0){
+            password += getChar(symbolsSet);
+            len--;
+        }
+    }
+    for(let i=0;i<strengthCount;i++){
+        boxes[i].style.backgroundColor = "#A4FFAF";
+    }
+
+    return password;
+}
+
+btn.addEventListener('click',()=>{
+    display.value = generatePassword();
+})
+
+copy.addEventListener('click',()=>{
+    if(display.value==""){
+        return;
+    }
+    
+    navigator.clipboard.writeText(display.value);
+    copy.innerText="copied";
+    setTimeout(() => {
+        copy.innerHTML=`<img src="vector.svg" alt="copy">`;
+    }, 2000);
+})
